a38a36e0e9533737b7148187dca45ac6f29d5039
IvyRoom <contato@ivyroom.com.br>
2024-08-30 09:30:51 -0300

    v3

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(cors({ origin: '*' }));
+app.listen(port);
 app.use(express.json());
 
 app.get('/login', (req, res) => {
-    res.send('Server is up and running.');
-});
-
-// app.post('/login', (req, res) => {
-//     const { email, senha } = req.body;
-//     if (email === 'lucasmac31@hotmail.com' && senha === '123') {
-//         res.json({ message: 'Login successful!', status: 'success' });
-//     } else {
-//         res.json({ message: 'Invalid credentials', status: 'fail' });
-//     }
-// });
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+    res.send('Surf is up.');
 });
 
