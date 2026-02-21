2f510f373e5026bd93bb0430765a0d6bd36decc1
IvyRoom <contato@machadogestao.com>
2026-02-21 16:29:23 -0300

    backend_v12.21

--- app.js
+++ app.js
@@ -19,6 +19,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Cria a aplicação necessária para receber HTTP Requests (Express).
 // Configura a aplicação para receber as requests de diferentes origens, inclusive do Localhost (Cors).
